a1f430a5e955151b65b26418834004e75170dfcb
PC-CILL <gmooocgel@gmail.com>
2023-07-19 12:35:12 +0200

    using Argon2

--- src/main/java/com/example/application/conf/SpringSecurity.java
+++ src/main/java/com/example/application/conf/SpringSecurity.java
@@ -1,22 +1,26 @@
 package com.example.application.conf;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@Slf4j
 public class SpringSecurity {
 
     @Bean
-    public static PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    public PasswordEncoder passwordEncoder() {
+        log.info("Creating Argon2PasswordEncoder");
+        return new Argon2PasswordEncoder(16, 32, 1, 1 << 14, 2);
     }
 
     @Bean
